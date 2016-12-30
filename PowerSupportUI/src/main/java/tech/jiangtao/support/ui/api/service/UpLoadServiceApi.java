70c0e2023a408d398dacaafbff85477658bd2aea
jiangTaoQuite <jiangtao103cp@163.com>
2016-12-30 18:39:50 +0800

    fix

--- PowerSupportUI/src/main/java/tech/jiangtao/support/ui/api/service/UpLoadServiceApi.java
+++ PowerSupportUI/src/main/java/tech/jiangtao/support/ui/api/service/UpLoadServiceApi.java
@@ -2,17 +2,19 @@ package tech.jiangtao.support.ui.api.service;
 
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
-import retrofit2.http.Field;
-import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 import rx.Observable;
 import tech.jiangtao.support.ui.model.FilePath;
-
 /**
- * Created by kevin on 28/12/2016.
- */
+ * Class: UpLoadServiceApi </br>
+ * Description: 文件上传 </br>
+ * Creator: kevin </br>
+ * Email: jiangtao103cp@gmail.com </br>
+ * Date: 29/12/2016 2:54 PM</br>
+ * Update: 29/12/2016 2:54 PM </br>
+ **/
 
 public interface UpLoadServiceApi {
   @Multipart @POST("file/fileupload") Observable<FilePath> upload(@Part MultipartBody.Part file);
