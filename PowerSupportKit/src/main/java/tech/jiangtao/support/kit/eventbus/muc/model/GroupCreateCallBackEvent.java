6bd6e675063831d267c7ebc3e329491ba892e7d8
jiangTaoQuite <jiangtao103cp@163.com>
2017-01-12 01:18:36 +0800

    fix

---
+++ PowerSupportKit/src/main/java/tech/jiangtao/support/kit/eventbus/muc/model/GroupCreateCallBackEvent.java
@@ -0,0 +1,14 @@
+package tech.jiangtao.support.kit.eventbus.muc.model;
+
+/**
+ * Class: GroupCreateCallBackEvent </br>
+ * Description: 群创建回调类 </br>
+ * Creator: kevin </br>
+ * Email: jiangtao103cp@gmail.com </br>
+ * Date: 12/01/2017 1:07 AM</br>
+ * Update: 12/01/2017 1:07 AM </br>
+ **/
+
+public class GroupCreateCallBackEvent {
+
+}
---
+++ PowerSupportKit/src/main/java/tech/jiangtao/support/kit/userdata/SimpleQueryGroup.java
@@ -0,0 +1,13 @@
+package tech.jiangtao.support.kit.userdata;
+
+/**
+ * Class: SimpleQueryGroup </br>
+ * Description: 封装群组查询功能 </br>
+ * Creator: kevin </br>
+ * Email: jiangtao103cp@gmail.com </br>
+ * Date: 12/01/2017 12:51 AM</br>
+ * Update: 12/01/2017 12:51 AM </br>
+ **/
+
+public class SimpleQueryGroup {
+}
