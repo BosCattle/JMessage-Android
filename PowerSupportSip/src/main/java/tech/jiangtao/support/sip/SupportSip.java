862d87b9fc25926542731187af3c609b12692db5
jiangTaoQuite <jiangtao103cp@163.com>
2017-01-11 13:14:28 +0800

    fix

---
+++ PowerSupportSip/src/main/java/tech/jiangtao/support/sip/SupportSip.java
@@ -0,0 +1,18 @@
+package tech.jiangtao.support.sip;
+
+import org.linphone.core.LinphoneCore;
+/**
+ * Class: SupportSip </br>
+ * Description: sip初始化 </br>
+ * Creator: kevin </br>
+ * Email: jiangtao103cp@gmail.com </br>
+ * Date: 11/01/2017 12:56 PM</br>
+ * Update: 11/01/2017 12:56 PM </br>
+ **/
+
+public class SupportSip {
+
+  public static void initialize(){
+
+  }
+}
