210a9cf654957c3fda626bdbe99afbd3c3479964
jiangTaoQuite <jiangtao103cp@163.com>
2017-03-16 16:19:47 +0800

    fix

--- app/src/main/java/com/china/epower/chat/app/PowerApp.java
+++ app/src/main/java/com/china/epower/chat/app/PowerApp.java
@@ -1,13 +1,7 @@
 package com.china.epower.chat.app;
 
 import android.app.Application;
-import android.content.Intent;
-import android.support.multidex.MultiDex;
 import java.util.UUID;
-import tech.jiangtao.support.ui.service.XMPPService;
-import com.pgyersdk.crash.PgyCrashManager;
-import com.squareup.leakcanary.LeakCanary;
-import tech.jiangtao.support.kit.init.SupportIM;
 import tech.jiangtao.support.ui.SupportUI;
 import work.wanghao.simplehud.SimpleHUD;
 
@@ -26,12 +20,9 @@ public class PowerApp extends Application {
 
   @Override public void onCreate() {
     super.onCreate();
-    //LeakCanary.install(this);
-    MultiDex.install(this);
-    PgyCrashManager.register(this);
     SimpleHUD.backgroundHexColor = "#FF4081";
     SupportUI.initialize(this, "dc-a4b8eb92-xmpp.jiangtao.tech.", UUID.randomUUID().toString(),
-        "139.162.73.105", 5222, "6e7ea2251ca5479d875916785c4418f1",
+        "192.168.43.164", 5222, "6e7ea2251ca5479d875916785c4418f1",
         "026eb8a2cb7b4ab18135a6a0454fd698", "http://ci.jiangtao.tech/fileUpload/");
   }
 }
