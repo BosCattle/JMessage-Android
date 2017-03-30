28b6e52b5903a5e1236f17b28d73725a035706af
jiangTaoQuite <jiangtao103cp@163.com>
2017-03-30 16:45:38 +0800

    fix

--- PowerSupportUI/src/main/java/tech/jiangtao/support/ui/activity/GroupCreateActivity.java
+++ PowerSupportUI/src/main/java/tech/jiangtao/support/ui/activity/GroupCreateActivity.java
@@ -47,133 +47,123 @@ import tech.jiangtao.support.ui.utils.RecyclerViewUtils;
  * Update: 2017/3/28 下午3:41 </br>
  **/
 public class GroupCreateActivity extends BaseActivity
-        implements EasyViewHolder.OnItemClickListener {
+    implements EasyViewHolder.OnItemClickListener {
 
-    @BindView(R2.id.tv_toolbar)
-    TextView mTvToolbar;
-    @BindView(R2.id.toolbar)
-    Toolbar mToolbar;
-    @BindView(R2.id.rv_constansList)
-    RecyclerView mConstantsList;
-    public static final String TAG = GroupCreateActivity.class.getSimpleName();
-    private ContactAdapter mContactAdapter;
-    private List<ConstrutContact> mConstrutContact;
-    private SimpleCGroup mSimpleCGroup;
-    private AppPreferences mAppPreferences;
-    private UserServiceApi mUserServiceApi;
+  @BindView(R2.id.tv_toolbar) TextView mTvToolbar;
+  @BindView(R2.id.toolbar) Toolbar mToolbar;
+  @BindView(R2.id.rv_constansList) RecyclerView mConstantsList;
+  public static final String TAG = GroupCreateActivity.class.getSimpleName();
+  private ContactAdapter mContactAdapter;
+  private List<ConstrutContact> mConstrutContact;
+  private SimpleCGroup mSimpleCGroup;
+  private AppPreferences mAppPreferences;
+  private UserServiceApi mUserServiceApi;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_create);
-        ButterKnife.bind(this);
-        init();
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_group_create);
+    ButterKnife.bind(this);
+    init();
+  }
 
+  public void init() {
+    mAppPreferences = new AppPreferences(this);
+    String name = null;
+    try {
+      name = mAppPreferences.getString("userJid");
+    } catch (ItemNotFoundException e) {
+      e.printStackTrace();
     }
-
-    public void init() {
-        mAppPreferences = new AppPreferences(this);
-        String name =null;
-        try {
-            name = mAppPreferences.getString("userJid");
-        }catch (ItemNotFoundException e){
-            e.printStackTrace();
-        }
-        name="vurtex@dc-a4b8eb92-xmpp.jiangtao.tech.";
-        LogUtils.d(TAG,name);
-        mUserServiceApi = ApiService.getInstance().createApiService(UserServiceApi.class);
-        setUpToolbar();
-        setUpAdapter();
-        mUserServiceApi.post(name).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread()).subscribe(list -> {
-            //TODO 这里解析JSON
-            for (Friends friends : list) {
-                LogUtils.d(TAG, friends.toString());
-            }
+    name = "vurtex@dc-a4b8eb92-xmpp.jiangtao.tech.";
+    LogUtils.d(TAG, name);
+    mUserServiceApi = ApiService.getInstance().createApiService(UserServiceApi.class);
+    setUpToolbar();
+    setUpAdapter();
+    mUserServiceApi.post(name)
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(list -> {
+          //TODO 这里解析JSON
+          for (Friends friends : list) {
+            LogUtils.d(TAG, friends.toString());
+          }
         }, new ErrorAction() {
-            @Override
-            public void call(Throwable throwable) {
-                super.call(throwable);
-                LogUtils.d(TAG,throwable.getLocalizedMessage());
-            }
+          @Override public void call(Throwable throwable) {
+            super.call(throwable);
+            LogUtils.d(TAG, throwable.getLocalizedMessage());
+          }
         });
+  }
 
-    }
+  /**
+   * Method: setUpAdapter </br>
+   * Description:  设置Adapter</br>
+   * Date: 15/01/2017 11:08 PM </br>
+   **/
+  private void setUpAdapter() {
+    mConstrutContact = new ArrayList<>();
+    mContactAdapter = new ContactAdapter(this, mConstrutContact);
+    mContactAdapter.setOnClickListener(this);
+    mConstantsList.addItemDecoration(RecyclerViewUtils.buildItemDecoration(this));
+    mConstantsList.setLayoutManager(
+        new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+    mConstantsList.setAdapter(mContactAdapter);
+  }
 
-    /**
-     * Method: setUpAdapter </br>
-     * Description:  设置Adapter</br>
-     * Date: 15/01/2017 11:08 PM </br>
-     **/
-    private void setUpAdapter() {
-        mConstrutContact = new ArrayList<>();
-        mContactAdapter = new ContactAdapter(this, mConstrutContact);
-        mContactAdapter.setOnClickListener(this);
-        mConstantsList.addItemDecoration(RecyclerViewUtils.buildItemDecoration(this));
-        mConstantsList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        mConstantsList.setAdapter(mContactAdapter);
-    }
+  public void setUpToolbar() {
+    mToolbar.setTitle("");
+    mTvToolbar.setText("创建群");
+    setSupportActionBar(mToolbar);
+    mToolbar.setNavigationIcon(R.mipmap.ic_arrow_back_white_24dp);
+    mToolbar.setNavigationOnClickListener(v -> this.finish());
+  }
 
-    public void setUpToolbar() {
-        mToolbar.setTitle("");
-        mTvToolbar.setText("创建群");
-        setSupportActionBar(mToolbar);
-        mToolbar.setNavigationIcon(R.mipmap.ic_arrow_back_white_24dp);
-        mToolbar.setNavigationOnClickListener(v -> this.finish());
-    }
+  @Override protected boolean preSetupToolbar() {
+    return false;
+  }
 
-    @Override
-    protected boolean preSetupToolbar() {
-        return false;
-    }
+  public static void startGroupCreate(Context context) {
+    Intent intent = new Intent(context, GroupCreateActivity.class);
+    context.startActivity(intent);
+  }
 
-    public static void startGroupCreate(Context context) {
-        Intent intent = new Intent(context, GroupCreateActivity.class);
-        context.startActivity(intent);
-    }
+  @Override public void onItemClick(int position, View view) {
 
-    @Override
-    public void onItemClick(int position, View view) {
+  }
 
-    }
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.menu_group_create, menu);
+    return true;
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_group_create, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_group_create) {
-            //检查是否有选择，创建群
-            String userJid = null;
-            String username = null;
-            try {
-                userJid = mAppPreferences.getString("userJid");
-                username = mAppPreferences.getString("username");
-                if (username == null) {
-                    username = StringSplitUtil.splitPrefix(userJid);
-                }
-            } catch (ItemNotFoundException e) {
-                e.printStackTrace();
-            }
-            GroupCreateParam mGroupCreateParam = new GroupCreateParam(username + "的群", userJid, null);
-            mSimpleCGroup.startCreateGroup(mGroupCreateParam, new GroupCreateCallBack() {
-                @Override
-                public void createSuccess() {
-                    //创建群聊成功
-                    GroupChatActivity.startGroupChat(GroupCreateActivity.this);
-                }
-
-                @Override
-                public void createFailed(String failedReason) {
-                    //创建群聊失败
-
-                }
-            });
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    if (item.getItemId() == R.id.menu_group_create) {
+      //检查是否有选择，创建群
+      String userJid = null;
+      String username = null;
+      try {
+        userJid = mAppPreferences.getString("userJid");
+        username = mAppPreferences.getString("username");
+        if (username == null) {
+          username = StringSplitUtil.splitPrefix(userJid);
         }
-        return true;
+      } catch (ItemNotFoundException e) {
+        e.printStackTrace();
+      }
+      GroupCreateParam mGroupCreateParam = new GroupCreateParam(username + "的群", userJid, null);
+      mSimpleCGroup.startCreateGroup(mGroupCreateParam, new GroupCreateCallBack() {
+        @Override public void createSuccess() {
+          //创建群聊成功
+          GroupChatActivity.startGroupChat(GroupCreateActivity.this);
+        }
+
+        @Override public void createFailed(String failedReason) {
+          //创建群聊失败
+
+        }
+      });
     }
+    return true;
+  }
 }
